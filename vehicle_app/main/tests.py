41f2513cea1539e6dc3037aa8a4c50d86745fb70
Sawyer Cherry <sawyeracherry@gmail.com>
2021-05-06 22:16:55 -0500

    fixed a small bug

--- vehicle_app/main/tests.py
+++ vehicle_app/main/tests.py
@@ -1,32 +1,38 @@
-import os
-from unittest import TestCase
+# import os
+# from unittest import TestCase
 
-from datetime import date
+# from datetime import date
  
-from vehicle_app import app, db, bcrypt
-from vehicle_app.models import Manufacturer, User, Vehicle
+# from vehicle_app import app, db, bcrypt
+# from vehicle_app.models import Manufacturer, User, Vehicle
 
-def login(client, username, password):
-    return client.post('/login', data=dict(
-        username=username,
-        password=password
-    ), follow_redirects=True)
+# def login(client, username, password):
+#     return client.post('/login', data=dict(
+#         username=username,
+#         password=password
+#     ), follow_redirects=True)
 
-def logout(client):
-    return client.get('/logout', follow_redirects=True)
+# def logout(client):
+#     return client.get('/logout', follow_redirects=True)
 
-def create_vehicle():
-    a1 = Manufacturer(manufacturer_name='General Motors')
-    b1 = Vehicle(
-        vin='3GCUKREC8JG183079',
-        model="2018 Silverado 1500 Z71 LT",
-    )
-    db.session.add(b1)
+# def create_vehicle():
+#     a1 = Manufacturer(manufacturer_name='General Motors')
+#     b1 = Vehicle(
+#         vin='3GCUKREC8JG183079',
+#         model="2018 Silverado 1500 Z71 LT",
+#     )
+#     db.session.add(b1)
 
-    a2 = Manufacturer(manufacturer_name="Ford Motor Company")
-    b2 = Vehicle(
-        vin='1FTFW1EFOBKD18803',
-        model="2011 Ford F150 Lariat"
-    )
-    db.session.add(b2)
-    db.session.commit()
+#     a2 = Manufacturer(manufacturer_name="Ford Motor Company")
+#     b2 = Vehicle(
+#         vin='1FTFW1EFOBKD18803',
+#         model="2011 Ford F150 Lariat"
+#     )
+#     db.session.add(b2)
+#     db.session.commit()
+#     def create_user():
+#     # Creates a user with username 'me1' and password of 'password'
+#     password_hash = bcrypt.generate_password_hash('password').decode('utf-8')
+#     user = User(username='me1', password=password_hash)
+#     db.session.add(user)
+#     db.session.commit()
